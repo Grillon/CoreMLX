0822aaa4e6879e1440179cb0c55a6277eb68a69d
Grillon <thierry.grillon@gmail.com>
2025-07-19 20:33:44 +0200

    phase1: integration modele dans le backend

--- backend/app/engine.py
+++ backend/app/engine.py
@@ -1,18 +1,27 @@
-from fastapi.responses import JSONResponse
-
+import requests
 def query_model(payload):
-    prompt = payload.get("messages", [{}])[-1].get("content", "")
-    response = {
-        "id": "mock-response",
-        "object": "chat.completion",
-        "choices": [{
-            "index": 0,
-            "message": {
-                "role": "assistant",
-                "content": f"You said: {prompt}"
-            },
-            "finish_reason": "stop"
-        }],
-        "usage": {"prompt_tokens": 0, "completion_tokens": 0, "total_tokens": 0}
+    prompt = payload.messages[-1].content
+    res = requests.post(
+        "http://localhost:8001/completion",
+        json={"prompt": prompt, "n_predict": 128},
+        timeout=60
+    )
+    data = res.json()
+    finish_reason = {
+    "limit": "length",
+    "stopword": "stop",
+    "user": "user_stop",
+    "": "unknown"
+}.get(data.get("stop_type", ""), "unknown")
+
+    return {
+        "content": data.get("content", ""),
+        "model": data.get("model", ""),
+        "tokens_predicted": data.get("tokens_predicted", 0),
+        "tokens_evaluated": data.get("tokens_evaluated", 0),
+        "timings": data.get("timings", {}),
+        "index": data.get("index",""),
+        "prompt_ms": data.get("timings", {}).get("prompt_ms", 0.0),
+        "prompt": data.get("prompt",""),
+        "finish_reason": finish_reason
     }
-    return JSONResponse(content=response)
--- backend/app/router.py
+++ backend/app/router.py
@@ -1,9 +1,36 @@
 from fastapi import APIRouter
+from .schemas import ChatRequest
 from .engine import query_model
-from .schemas import ChatRequest  # si séparé
 
 router = APIRouter()
 
 @router.post("/v1/chat/completions")
-async def chat_completion(payload: ChatRequest):
-    return query_model(payload.dict())
+def chat_completion(payload: ChatRequest):
+    raw = query_model(payload)
+
+    return {
+        "id": "coremlx-response",
+        "object": "chat.completion",
+        "model": raw["model"]+" en réalité mistral7B_Q4_K_M",
+        "choices": [{
+            "index": raw["index"],
+            "message": {
+                "role": "assistant",
+                "content": raw["content"]
+            },
+            "finish_reason": raw["finish_reason"]
+        }],
+        "usage": {
+            "prompt": raw["prompt"],
+            "prompt_tokens": raw["tokens_evaluated"],
+            "completion_tokens": raw["tokens_predicted"],
+            "total_tokens": raw["tokens_predicted"]+raw["tokens_evaluated"]
+        },
+        "timings": {
+        "prompt_ms": raw["prompt_ms"],
+        "predicted_ms": raw["timings"].get("predicted_ms", 0.0),
+        "latency_ms": raw["timings"].get("predicted_ms", 0.0)
+}
+
+    }
+
